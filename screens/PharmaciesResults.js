b685bafbd309a2d7bfbb8372e1b0dbead7fd081e
Ay0ub N0uri <ayoub.nouri105@gmail.com>
2023-05-19 03:08:40 +0100

    add map section

--- screens/PharmaciesResults.js
+++ screens/PharmaciesResults.js
@@ -1,4 +1,4 @@
-import { Box, Center, FlatList } from "native-base";
+import { Box, Center, FlatList, Pressable, useColorModeValue } from "native-base";
 import { RootContext } from "../store/context/root-context";
 import { useContext, useEffect, useState } from "react";
 import { init } from "ws";
@@ -8,11 +8,14 @@ import PharmacyItem from "../components/Pharmacies/PharmacyItem";
 import PharmacySummary from "../components/Pharmacies/PharmacySummary";
 import ErrorOverlay from "../components/core/ErrorOverlay";
 import NothingFound from "../components/core/NothingFound";
+import { SceneMap, TabView } from "react-native-tab-view";
+import { Dimensions, StatusBar } from "react-native";
+import Animated from "react-native-reanimated";
 
 
-const PharmaciesResults = ({ route }) => {
+
+const ListView = ({ route }) => {
     const { city, zone, night, day } = route.params;
-    const { themeMode } = useContext(RootContext);
     const [pharmacies, setPharmacies] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState('');
@@ -40,8 +43,7 @@ const PharmaciesResults = ({ route }) => {
 
     useEffect(() => {
         init();
-    }, [])
-
+    }, [pharmacies])
     const renderPharmacyItem = (itemData) => {
         const pharmacy = itemData.item;
         const pharmacyProps = {
@@ -54,12 +56,10 @@ const PharmaciesResults = ({ route }) => {
             longitude: pharmacy.longitude,
             zone: pharmacy.zone,
         }
-        // console.log(pharmacy);
-        return <PharmacyItem {...pharmacyProps} />
+        return <PharmacyItem key={pharmacy._id} {...pharmacyProps} />
     }
-
     return (
-        <Box bg={themeMode.current.bgColor} flex={1}>
+        <>
             {
                 error ? <ErrorOverlay message={error} onPress={() => {
                     setError(null);
@@ -73,6 +73,81 @@ const PharmaciesResults = ({ route }) => {
                                 <FlatList data={pharmacies} keyExtractor={item => item._id} renderItem={renderPharmacyItem} />
                             </>
             }
+        </>
+    )
+}
+
+const MapView = () => {
+    return (
+        <Center flex={1} my="4">
+            This is map view
+        </Center>
+    )
+}
+
+const initialLayout = {
+    width: Dimensions.get('window').width
+};
+
+
+
+const PharmaciesResults = ({ route }) => {
+    const { themeMode } = useContext(RootContext);
+
+
+
+
+    const [index, setIndex] = useState(0);
+    const [routes, setRoutes] = useState([{
+        key: 'listView',
+        title: 'List View'
+    }, {
+        key: 'mapView',
+        title: 'Map View'
+    },
+    ])
+
+    const renderScene = SceneMap({
+        listView: () => <ListView route={route} />,
+        mapView: MapView,
+    });
+
+    const renderTabBar = props => {
+        const inputRange = props.navigationState.routes.map((x, i) => i);
+        return <Box flexDirection="row">
+            {props.navigationState.routes.map((route, i) => {
+                const opacity = props.position.interpolate({
+                    inputRange,
+                    outputRange: inputRange.map(inputIndex => inputIndex === i ? 1 : 0.5)
+                });
+                const color = index === i ? useColorModeValue('#000', '#e5e5e5') : useColorModeValue('#1f2937', '#a1a1aa');
+                const borderColor = index === i ? 'cyan.500' : useColorModeValue('coolGray.200', 'gray.400');
+                return <Box borderBottomWidth="3" borderColor={borderColor} flex={1} alignItems="center" p="3" cursor="pointer">
+                    <Pressable onPress={() => {
+                        console.log(i);
+                        setIndex(i);
+                    }}>
+                        <Animated.Text style={{
+                            color
+                        }}>{route.title}</Animated.Text>
+                    </Pressable>
+                </Box>;
+            })}
+        </Box>;
+    };
+
+    return (
+        <Box bg={themeMode.current.bgColor} flex={1}>
+            <TabView navigationState={{
+                index,
+                routes
+            }} renderScene={renderScene} renderTabBar={renderTabBar} onIndexChange={setIndex} initialLayout={initialLayout} style={{
+                marginTop: StatusBar.currentHeight
+            }} />
+
+
+
+
         </Box>
     )
 }
